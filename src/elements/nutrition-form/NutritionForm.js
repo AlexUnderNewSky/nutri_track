8505267363c5a0751bce2f7cf5ad7d3e2dfa78cd
Serhii Sosnytskyi <sosnitskij2021@gmail.com>
2024-03-24 09:09:55 +0200

    add Nutrition Form

---
+++ src/elements/nutrition-form/NutritionForm.js
@@ -0,0 +1,105 @@
+import { useState } from "react";
+import { Form } from "react-bootstrap";
+import { Button } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import "./NutritionForm.css";
+import { createNutrition } from "../../../services/nutrition-service";
+import { v4 as uuidv4 } from 'uuid';
+
+export function NutritionForm() {
+  const navigate = useNavigate();
+
+  const [error, setError] = useState("");
+  const [nutrition, setNutrition] = useState({
+    description: '',
+    protein: 0,
+    fat: 0,
+    carbs: 0,
+  });
+
+  const onInputChange = (e) => {
+    setNutrition((prevState) => {
+      let currentName = e.target.name;
+      let currentValue = e.target.value;
+
+      return {
+        ...prevState,
+        [currentName]: isNaN(currentValue) ? currentValue : parseFloat(currentValue),
+      };
+    });
+  };
+
+  const onFormSubmit = (e) => {
+    e.preventDefault();
+
+    try {
+        createNutrition({id: uuidv4(), ...nutrition})
+      .then(() => {
+        navigate(`/`);
+      })
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
+  return (
+    <div className="nutrition-form-wrapper">
+      <Form onSubmit={onFormSubmit}>
+        {error && <span className="text-danger">{error}</span>}
+        <Form.Group className="mb-3" controlId="name">
+          <Form.Label>Description</Form.Label>
+          <Form.Control
+            type="text"
+            name="description"
+            value={nutrition.description}
+            placeholder="Enter Description"
+            onChange={onInputChange}
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="name">
+          <Form.Label>Protein</Form.Label>
+          <Form.Control
+            type="number"
+            name="protein"
+            value={nutrition.protein}
+            placeholder="Enter Protein"
+            onChange={onInputChange}
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="name">
+          <Form.Label>Fat</Form.Label>
+          <Form.Control
+            type="number"
+            name="fat"
+            value={nutrition.fat}
+            placeholder="Enter Fat"
+            onChange={onInputChange}
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="name">
+          <Form.Label>Carbs</Form.Label>
+          <Form.Control
+            type="number"
+            name="carbs"
+            value={nutrition.carbs}
+            placeholder="Enter Carbs"
+            onChange={onInputChange}
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="name">
+          <Form.Label>Kcal</Form.Label>
+          <Form.Control
+            type="number"
+            name="kcal"
+            value={nutrition.kcal}
+            placeholder="Enter Kcal"
+            onChange={onInputChange}
+          />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Add Nutrition
+        </Button>
+      </Form>
+    </div>
+  );
+}
