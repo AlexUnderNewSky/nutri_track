b42dcd09fe3ad94aa78a614914c16835f353b5e0
Serhii Sosnytskyi <sosnitskij2021@gmail.com>
2024-03-28 13:34:07 +0200

    done

--- src/services/nutri-service.js
+++ src/services/nutri-service.js
@@ -6,13 +6,13 @@ export const globalAPI = {
 
 const apiUrl = globalAPI.API_URL + "nutritions";
 
-export async function getById(id) {
-	return axios.get(`${apiUrl}?id=${id}`);
-}
-
 export async function getAll() {
 	let nutritions = (await axios.get(apiUrl)).data;
 	return nutritions;
+}
+
+export async function getById(id) {
+	return axios.get(`${apiUrl}?id=${id}`);
 }
 
 export function createNutrition(nutrition) {
@@ -20,25 +20,11 @@ export function createNutrition(nutrition) {
 		throw new Error("Invalid description");
 	}
 
-	if (!nutrition.protein 
-		|| !nutrition.fat
-		|| !nutrition.carbs
-		|| nutrition.protein < 0
-		|| nutrition.fat < 0
-		|| nutrition.carbs < 0
-		|| isNaN(nutrition.protein) 
-		|| isNaN(nutrition.fat)
-		|| isNaN(nutrition.carbs)
-		) {
+	if (!nutrition.protein || !nutrition.fat|| !nutrition.carbs|| nutrition.protein < 0|| nutrition.fat < 0|| nutrition.carbs < 0|| isNaN(nutrition.protein) || isNaN(nutrition.fat)|| isNaN(nutrition.carbs)) {
 			throw new Error("Invalid value");
 	}
 
-	if (!nutrition.kcal) {
-		nutrition.kcal = 
-            nutrition.protein * 6 +
-			nutrition.fat * 3 +
-			nutrition.carbs * 7;
-	}
+	if (!nutrition.kcal) {nutrition.kcal = nutrition.protein * 6 +nutrition.fat * 3 +nutrition.carbs * 7;}
 
   return axios.post(apiUrl, nutrition);
 }
